cca814a13f70c99c5ee31d67b14973f751fffce7
Prasit Gebsaap <prasit.gebsaap@outlook.com>
2026-09-24 13:57:02 +0700

    Add laravel 13 support

--- src/OmiseWebhooksProfile.php
+++ src/OmiseWebhooksProfile.php
@@ -10,6 +10,6 @@ class OmiseWebhooksProfile implements WebhookProfile
 {
     public function shouldProcess(Request $request): bool
     {
-        return ! WebhookCall::where('name', 'omise')->where('payload->id', $request->get('id'))->exists();
+        return ! WebhookCall::where('name', 'omise')->where('payload->id', $request->input('id'))->exists();
     }
 }
